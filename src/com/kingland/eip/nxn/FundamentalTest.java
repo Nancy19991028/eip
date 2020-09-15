7127765fa461f270aa3b7b242ccd4269bafbc668
nanni-meinv <nxn991028@163.com>
2020-09-15 01:47:36 +0000

    this is my first practice

--- src/com/kingland/eip/nxn/FundamentalTest.java
+++ src/com/kingland/eip/nxn/FundamentalTest.java
@@ -1,98 +1,102 @@
+/**
+ * Copyright 2020 Kingland Systems Corporation. All Rights Reserved.
+ */
 package com.kingland.eip.nxn;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 /**
- * Print "Hello World".
+ * Four exercises with the Java foundation
  */
-
 public class FundamentalTest {
     public static void main(String args[]) {
+        //Print "Hello World".
         System.out.println("hello world");
-    }
-
-    /**
-     * Print the pattern below
-     */
-
-    public static class Pattern{
-        public static void main(String args[]) {
-            String str = new String();
-            str = "**********";
-            for(int i = str.length() - 1; i >= 0; i--){
-                System.out.println(str);
-                str = str.substring(0,i);
+        //Set the number of stars and print.
+        final int num = 10;
+        String star = "*";
+        for (int i = 0; i < num; i++) {
+            for (int j = num - i; j > 0; j--) {
+                System.out.print(star);
             }
-            }
-            }
-
-    /**
-     * Print the current date and time
-     */
-    static class TeLocalDate {
-
-        public static void main(String[] args) {
-            //Date, get year month day
-            LocalDate ld = LocalDate.now();
-            System.out.println(ld);
-
-            //Time, gain time
-            LocalTime lt = LocalTime.now();
-            System.out.println(lt); //获得当前时间
+            System.out.println();
         }
-    }
-
-    /**
-     * Output the abbreviation for the corresponding month
-     */
-    static class Month {
-    public static final int Month = 10;
-    public static final String[] MonthName = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-
-    public static void main(String[] args) {
-        System.out.println(MonthName[Month - 1]);
-    }
-}
-
-    /**
-     * Determine if it is even and print
-     */
-    static class Method{
-    public static void main(String[] args){
+        //Print the current date and time below the pattern.
+        System.out.println("localTime: " + LocalDate.now() + " " + LocalTime.now());
+        //Set the initial month to October.
+        printMonth(10);
+        //Returns the length of "Good Morning" to determine if the length is even.
         String str = "Good Morning";
-        int len = length(str);
-        if (len % 2 == 0) {
-            System.out.println(len);
-        }
-        else if(len%2==1){
-            System.out.println("String is jishu!");
+        int length = stringMethod(str);
+        if (length % 2 == 0)
+            System.out.println(length);
+        else
+            System.out.println("String is odd");
+    }
+
+    /**
+     * An abbreviation for the corresponding month printed in different months.
+     *
+     * @param num month
+     */
+    public static void printMonth(int num) {
+        switch (num) {
+            case 1:
+                System.out.println("Jan");
+                break;
+            case 2:
+                System.out.println("Feb");
+                break;
+            case 3:
+                System.out.println("Mar");
+                break;
+            case 4:
+                System.out.println("Apr");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("Jun");
+                break;
+            case 7:
+                System.out.println("Jul");
+                break;
+            case 8:
+                System.out.println("Aug");
+                break;
+            case 9:
+                System.out.println("Sept");
+                break;
+            case 10:
+                System.out.println("Oct");
+                break;
+            case 11:
+                System.out.println("Nov");
+                break;
+            default:
+                System.out.println("Dec");
+                break;
         }
     }
 
-
-
-        public static int length(String s){
-            if(s.indexOf(" ")!=-1&&"G".equals(s.substring(0,1))){
-                String[] newStr = s.split(" ");
-                for (String string : newStr) {
-                    System.out.println(string);
+    /**
+     * Split the string and determine if its length is odd or even.
+     *
+     * @param str string
+     * @return String length
+     */
+    public static int stringMethod(String str) {
+        if (str != null && !str.isEmpty()) {
+            if (str.contains(" ") || str.startsWith("G")) {
+                String[] strArray = str.split(" ");
+                for (String item : strArray) {
+                    System.out.println(item);
                 }
             }
-            return s.length();
-
+            return str.length();
+        }
+        return -1;
     }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
