9c20ab78479f305d1e0d3bea9d5899ee96b60007
nanni-meinv <nxn991028@163.com>
2020-09-23 10:07:02 +0000

    my practice

---
+++ src/com/kingland/eip/practice/TestLogJava/src/Main.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2020 Kingland Systems Corporation. All Rights Reserved.
+ */
+package com.kingland.eip.practice.TestLogJava.src;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // set corresponding messages and exception
+        String consoleMessage = "Message";
+        String fileMessage = "Hello, Message";
+        String exception = "Exception: NullPointerException";
+
+        // create corresponding loggers
+        Logger fileLogger = (Logger) new FileLogger
+                .Builder()
+                .setMinLength(10)
+                .setMaxLength(50)
+                .build();
+        fileLogger.logger("Exception file");
+        fileLogger.logger("Exception file", "Test minimum length");
+
+        Logger consoleLogger = (Logger) new ConsoleLogger
+                .Builder()
+                .setMinLength(5)
+                .setMaxLength(10)
+                .build();
+
+        consoleLogger.logger("Exception console");
+        consoleLogger.logger("Exception console", "Console test");
+    }
+}
