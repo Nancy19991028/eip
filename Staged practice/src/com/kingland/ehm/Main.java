9c8b6696e8aa552a7f589c2a42e5c625774a9f91
nanni-meinv <nxn991028@163.com>
2020-09-28 09:39:18 +0000

    My project codes

---
+++ Staged practice/src/com/kingland/ehm/Main.java
@@ -0,0 +1,23 @@
+package com.kingland.ehm;
+
+import com.kingland.ehm.buffer.BaseBuffer;
+import com.kingland.ehm.buffer.LinkedListBuffer;
+import com.kingland.ehm.loader.BaseLoader;
+import com.kingland.ehm.loader.ConsoleLoader;
+import com.kingland.ehm.sender.BaseSender;
+import com.kingland.ehm.sender.ConsoleSender;
+
+public class Main {
+    public static void main(String[] args) {
+        BaseBuffer buffer = new LinkedListBuffer<>(5);
+
+        BaseLoader loader = new ConsoleLoader(buffer);
+        BaseSender sender = new ConsoleSender(buffer);
+
+        loader.setPriority(10);
+        sender.setPriority(1);
+
+        loader.start();
+        sender.start();
+    }
+}
