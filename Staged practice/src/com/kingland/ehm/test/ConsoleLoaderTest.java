1bd57e3fe4eb8634dd072682b35096863a42322e
nanni-meinv <nxn991028@163.com>
2020-10-14 07:37:54 +0000

    My project codes

---
+++ Staged practice/src/com/kingland/ehm/test/ConsoleLoaderTest.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2020 Kingland Systems Corporation. All Rights Reserved.
+ */
+
+package com.kingland.ehm.test;
+
+import org.junit.jupiter.api.Test;
+
+class ConsoleLoaderTest {
+
+    @Test
+    void testGetData() {
+    }
+}
